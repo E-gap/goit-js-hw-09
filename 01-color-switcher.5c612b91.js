9e7f8ae881eb03a2695fdd1f43ccb51428a8ec4b
E-gap <106596181+E-gap@users.noreply.github.com>
2022-10-24 12:01:54 +0000

    Deploying to gh-pages from @ E-gap/goit-js-hw-09@320f429750eeb20640e9acf1c37ed1fb385883be 🚀

---
+++ 01-color-switcher.5c612b91.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),r=document.querySelector("body");let o=0,a=!1;t.addEventListener("click",(()=>{a=!0,o=setInterval((()=>{r.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`,t.setAttribute("disabled",!0)}),1e3)})),e.addEventListener("click",(()=>{a=!1,clearInterval(o),t.removeAttribute("disabled")}));
+//# sourceMappingURL=01-color-switcher.5c612b91.js.map
